8fdbe4eabeb1a283324613d2f1b93c7187fa8f19
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2024-08-17 22:35:37 +0530

    Update lowerAndUpperbound.js
    end val change

--- dsaFromStratch/binarySearch/lowerAndUpperbound.js
+++ dsaFromStratch/binarySearch/lowerAndUpperbound.js
@@ -4,9 +4,10 @@ function lowerBound(arr, target) {
   let begin = 0
   let end = arr.length - 1
   let mid = Math.floor(begin + (end - begin) / 2)
-  while (begin < end) {
+  while (begin <= end) {
     if (arr[mid] >= target) {
-      end = answer = mid
+      end  = mid - 1
+      answer = mid
     } else begin = mid + 1
     mid = Math.floor(begin + (end - begin) / 2)
   }
