381e18c44b53e88113739186906ba70713a6a645
Niladri <niladrisengit12@gmail.com>
2024-08-19 12:41:42 +0530

    refactor: Fix upperBound function in binary search implementation

--- dsaFromStratch/binarySearch/lowerAndUpperbound.js
+++ dsaFromStratch/binarySearch/lowerAndUpperbound.js
@@ -13,5 +13,22 @@ function lowerBound(arr, target) {
   }
   return answer
 }
-const index = lowerBound([3, 5, 8, 15, 19], 11)
+
+function upperBound(arr, target) {
+  let begin = 0
+  let end = arr.length - 1
+  let answer = arr.length
+  let mid = Math.floor(begin + (end - begin) / 2)
+  while (begin <= end) {
+    if (arr[mid] > target) {
+      answer = mid
+      end = mid - 1
+    } else {
+      begin = mid + 1
+    }
+    mid = Math.floor(begin + (end - begin) / 2)
+  }
+  return answer
+}
+const index = upperBound([2,3,6,7,8,8,8,11,11,11,12], 10)
 console.log(index)
